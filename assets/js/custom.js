ba0e0a47f3cb2311ad7048cf0f2a2be85d1fa1c9
Gajendra Kumar <gajjida@gmail.com>
2021-09-16 10:50:52 +0530

    index file changes ,

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -303,6 +303,14 @@ function changeSlide() {
 
 
 
+const slidesElm = document.querySelectorAll(".slider-container .slide-box");
+
+Array.from(slidesElm).forEach((slide) => {
+  slide.addEventListener("click", () => {
+    Array.from(slidesElm).forEach((sld) => sld.classList.remove("active"));
+    slide.classList.add("active");
+  });
+});
 
 
 
